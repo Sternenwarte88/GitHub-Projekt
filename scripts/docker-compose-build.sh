24c89b2e32f9731a810e80e3f1470200d3dab6f8
Sternenwarte88 <s.loecher.slc@gmail.com>
2025-05-27 20:05:45 +0200

    fixing worflow file

--- scripts/docker-compose-build.sh
+++ scripts/docker-compose-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-DOCKER_PATH="../docker"
-BACKEND_PATH="../app/backend"
+DOCKER_PATH="./docker"
+BACKEND_PATH="./app/backend"
 
 VERSION=$(node -p "require('${BACKEND_PATH}/package.json').version")
 
 echo VERSION=$VERSION > "${DOCKER_PATH}/.env"
 
-docker compose -f ../docker/docker-compose.yaml build 
+docker compose -f "${DOCKER_PATH}/docker-compose.yaml" build 
 docker push -a wusabinga/projekt-backend 
 docker push -a wusabinga/projekt-mysql 
